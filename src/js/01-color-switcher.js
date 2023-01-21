b27c2e6448cec09d2f7f88751e7790c38c995548
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-01-21 10:20:07 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+// шо зі стилями???
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let isInterval = false;
+let intervalId;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  if (!isInterval) {
+    changeBodyBgColor();
+
+    intervalId = setInterval(changeBodyBgColor, 1000);
+    isInterval = true;
+  }
+}
+
+function onStopBtnClick() {
+  if (isInterval) {
+    clearInterval(intervalId);
+    isInterval = false;
+  }
+}
+
+function changeBodyBgColor() {
+  const randomColor = getRandomHexColor();
+
+  refs.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
