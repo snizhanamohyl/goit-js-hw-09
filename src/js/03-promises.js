ef048a24eb9651c39ae3b840218b7c9c7f026b86
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-01-23 23:58:40 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -31,7 +31,7 @@ function onformSubmit(event) {
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-  return (promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -39,5 +39,5 @@ function createPromise(position, delay) {
         reject(`❌ Rejected promise ${position} in ${delay}ms`);
       }
     }, delay);
-  }));
+  });
 }
